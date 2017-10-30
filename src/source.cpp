6bfdae5a261a9bfbdf85db88a70fa320cebaff44
root <root@localhost>
2017-10-30 20:15:45 +1030

    a

--- src/source.cpp
+++ src/source.cpp
@@ -6,16 +6,17 @@ using namespace std;
 int main()
 {
 	Node node = Node();
-	node.output.weight = 1;
 	Node node2 = Node();
-	node2.output.weight = 1;
 	Node node3 = Node(2);
-	node.data.set(1.0);
-	node2.data.set(2.0);
-	node3.input[0] = &node.output;
-	node3.input[1] = &node2.output;
+	
+	Link a = Link(node.output, node3.input[0]);
+	Link b = Link(node2.output, node3.input[1]);
+	
+	node.output->data.set(1.0);
+	node2.output->data.set(2.0);
+	
 	node3.calc();
-	cout << "hello node " << node3.data.get<double>() << endl;
+	cout << "hello node " << node3.output->data.get<double>() << endl;
 	int junk;
 	cin >> junk;
 	return 0;
