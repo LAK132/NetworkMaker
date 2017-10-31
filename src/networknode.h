628d0a2e618e40bd07dfe55284048faca1592907
root <root@localhost>
2017-10-31 23:01:09 +1030

    added Network class

--- src/networknode.h
+++ src/networknode.h
@@ -4,54 +4,62 @@ using std::vector;
 #ifndef NETWORK_NODES_H
 #define NETWORK_NODES_H
 
-class Synapse
+class Network
 {
 private:
-    double val;
+	vector<Node*> nodetree;
+	vector<Link*> linktree;
 public:
-    double weight;
-    Socket* sock;
-    Synapse(Socket* s)
-    {
-        sock = s;
-        weight = 1.0;
-    }
-    void set(double value)
-    {
-        val = value;
-    }
-    double get()//Socket* sock)
-    {
-        if (sock->link == 0 || sock->link->from == sock)
-        {
-            return val * weight;
-        }
-        else
-        {
-            Socket* other = sock->link->from->parent->output;
-            double rtn = other->data.getr<Synapse>().get();
-            return rtn * weight;
-        }
-    }
-};
+	class Synapse
+	{
+	private:
+		double val;
+	public:
+		double weight;
+		Socket *sock;
+		Synapse(Socket * s)
+		{
+			sock = s;
+			weight = 1.0;
+		}
+		void set(double value)
+		{
+			val = value;
+		}
+		double get()			// Socket* sock)
+		{
+			if (sock->link == 0 || sock->link->from == sock)
+			{
+				return val * weight;
+			}
+			else
+			{
+				Socket *other = sock->link->from->parent->output;
+				double rtn = other->data.getr<Synapse>().get();
+				return rtn * weight;
+			}
+		}
+	};
 
-class Neuron
-{
-public:
-    Node* node;
-    Neuron(Node* n)
-    {
-        node = n;
-    }
-    void calc()
-    {
-        double temp = 0.0;
-        for (auto it = node->input.begin(); it != node->input.end(); it++) {
-            //printf("temp %d\n",(int)temp);
-            temp += (*it)->data.getr<Synapse>().get();//*it);
-        }
-        node->output->data.getr<Synapse>().set(temp);
-    }
+	class Neuron
+	{
+	public:
+		Node * node;
+		Neuron(Node * n)
+		{
+			node = n;
+		}
+		void calc()
+		{
+			double temp = 0.0;
+			for (auto it = node->input.begin(); it != node->input.end(); it++)
+			{
+				// printf("temp %d\n",(int)temp);
+				temp += (*it)->data.getr<Synapse>().get();	// *it);
+			}
+			node->output->data.getr<Synapse>().set(temp);
+		}
+	};
 };
 
 #endif
